a38109a53195529081cb11f4ed21b2d7ad4948b7
Aleksandr Klimov <70091133+blik1984@users.noreply.github.com>
2025-04-22 19:25:57 +0300

    Update Library.java

--- src/com/edu/training/jc/library/Library.java
+++ src/com/edu/training/jc/library/Library.java
@@ -31,6 +31,36 @@ public class Library {
 			this.library.add(book);
 		
 		}
+	public void del(int id) {
+		for (int i = 0; i < library.size(); i++) {
+			if (library.get(i).getId() == id) {
+				library.remove(i);
+				return;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Library [library=" + library + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(library);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Library other = (Library) obj;
+		return Objects.equals(library, other.library);
+	}
 	}
 
 
